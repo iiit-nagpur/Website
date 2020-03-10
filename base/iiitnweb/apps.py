1d1f2a741d79a3bbe5f8e94ceb2f48fbaef80620
Arun <arun49804@gmail.com>
2020-03-10 23:34:31 +0530

    Name change

--- base/iiitnweb/apps.py
+++ base/iiitnweb/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class IiitnConfig(AppConfig):
-    name = 'iiitn'
+class IiitnwebConfig(AppConfig):
+    name = 'iiitnweb'
